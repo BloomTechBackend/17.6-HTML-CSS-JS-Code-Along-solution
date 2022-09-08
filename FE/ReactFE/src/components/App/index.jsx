7af1e39ed10079cae90306242e6ddd27f0497616
Tom Tarpey <tomtarpeydev@gmail.com>
2022-09-08 11:24:02 +0100

    initial commit

---
+++ FE/ReactFE/src/components/App/index.jsx
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import api from "../../api/contacts";
+import "./App.css";
+import Header from "../Header";
+import AddContact from "../AddContact";
+import ContactList from "../ContactList";
+import ContactDetail from "../ContactDetail";
+import EditContact from "../EditContact";
+
+function App() {
+  const [contacts, setContacts] = useState([]);
+
+  //RetrieveContacts
+  const retrieveContacts = async () => {
+    const response = await api.get("http://localhost:8080/contacts/");
+    return response.data;
+  };
+
+  const addContactHandler = async (contact) => {
+    contact.phone_numbers = contact.phone_numbers.split(",");
+    for (let i = 0; i < contact.phone_numbers.length; i++) {
+      contact.phone_numbers[i] = contact.phone_numbers[i].trim();
+    }
+    console.log(contact);
+
+
+    const request = {
+      ...contact
+    };
+
+    const response = await api.post("http://localhost:8080/contacts/", request);
+    console.log(response);
+    setContacts([...contacts, response.data]);
+  };
+
+  const updateContactHandler = async (contact) => {
+
+    contact.phone_numbers = contact.phone_numbers.split(",");
+    console.log(contact.phone_numbers);
+    for (let i = 0; i < contact.phone_numbers.length; i++) {
+      contact.phone_numbers[i] = contact.phone_numbers[i].trim();
+    }
+
+    console.log(contact);
+
+    const response = await api.put(`http://localhost:8080/contacts/${contact.id}`, contact);
+    const { id } = response.data;
+    setContacts(
+      contacts.map((contact) => {
+        return contact.id === id ? { ...response.data } : contact;
+      })
+    );
+    getAllContacts();
+  };
+
+  const removeContactHandler = async (id) => {
+    await api.delete(`http://localhost:8080/contacts/${id}`);
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  const getAllContacts = async () => {
+    const allContacts = await retrieveContacts();
+    if (allContacts) setContacts(allContacts);
+  };
+
+  useEffect(() => {
+    const getAllContacts = async () => {
+        const allContacts = await retrieveContacts();
+        if (allContacts) setContacts(allContacts);
+      };
+    getAllContacts();
+  }, []);
+
+  return (
+    <div className="ui container">
+      <Router>
+        <Header />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(props) => (
+              <ContactList
+                {...props}
+                contacts={contacts}
+                getContactId={removeContactHandler}
+              />
+            )}
+          />
+          <Route
+            path="/add"
+            render={(props) => (
+              <AddContact {...props} addContactHandler={addContactHandler} />
+            )}
+          />
+
+          <Route
+            path="/edit"
+            render={(props) => (
+              <EditContact
+                {...props}
+                updateContactHandler={updateContactHandler}
+              />
+            )}
+          />
+
+          <Route path="/contact/:id" component={ContactDetail} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
